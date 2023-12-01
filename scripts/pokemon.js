45c6688b29544d851d2e331815c191cbdee11cda
Miles <alexlmcdonald@outlook.com>
2023-12-01 15:58:29 -0700

    Finished and fixed w05-tasks! ☺

---
+++ scripts/pokemon.js
@@ -0,0 +1,20 @@
+// not actually a javascript file... copy and paste this in a website inspect console to see it work!
+
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+let results = null;
+async function getPokemon(url) {
+  const response = await fetch(url);
+  //check to see if the fetch was successful
+  if (response.ok) {
+    // the API will send us JSON...but we have to convert the response before we can use it
+    // .json() also returns a promise...so we await it as well.
+    const data = await response.json();
+    doStuff(data);
+  }
+}
+function doStuff(data) {
+  results = data;
+  console.log("first: ", results);
+}
+getPokemon(url);
+console.log("second: ", results);
